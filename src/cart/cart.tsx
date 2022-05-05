435d113866f4c6d03b2f5948b8086e42fe0d70ff
IbraheemLanre <s2001108@edu.bc.fi>
2022-05-05 23:46:45 +0300

    removed package-lock.json folder

---
+++ src/cart/cart.tsx
@@ -0,0 +1,35 @@
+import CartItem from "../cartItem/cartItem";
+
+// Styles
+import { Wrapper } from "./cart.styles";
+
+//Types
+import { CartItemType } from "../App";
+
+type Props = {
+  cartItems: CartItemType[];
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const Cart = ({ cartItems, addToCart, removeFromCart }: Props) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+  return (
+    <Wrapper>
+      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <p>You've have an empty cart.</p> : null}
+      {cartItems.map((item) => (
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+    </Wrapper>
+  );
+};
+
+export default Cart;
